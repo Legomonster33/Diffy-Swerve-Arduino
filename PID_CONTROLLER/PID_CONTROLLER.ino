7a8648baf02d82f8e0eb1d206134bceeb95a6663
Legomonster33 <siglevan@gmail.com>
2023-04-23 15:53:46 -0400

    Update PID_CONTROLLER.ino

--- PID_CONTROLLER/PID_CONTROLLER.ino
+++ PID_CONTROLLER/PID_CONTROLLER.ino
@@ -1,4 +1,4 @@
-float Speed[8],PotVal[8],Rpm[8],Rpm2[8],Rpm3[8],TachoTimer[8],TachoValue[8],OldTachoValue[8],Rpm1[8],ControlValue[12],ProcessValue[12],SetPoint[12],P[12],I[12],D[12],MaxSummativeError[12],Error[12],LastError[12],ErrorSummative[12],PreviousTime[12],DeltaTime[12],Proportional,Integral,Derivative;
+float Rpm6[8],Rpm5[8],Rpm4[8],Speed[8],PotVal[8],Rpm[8],Rpm2[8],Rpm3[8],TachoTimer[8],TachoValue[8],OldTachoValue[8],Rpm1[8],ControlValue[12],ProcessValue[12],SetPoint[12],P[12],I[12],D[12],MaxSummativeError[12],Error[12],LastError[12],ErrorSummative[12],PreviousTime[12],DeltaTime[12],Proportional,Integral,Derivative;
 
 void PID(int Idx){
   Error[Idx] = SetPoint[Idx] - ProcessValue[Idx];
@@ -18,18 +18,32 @@ void PID(int Idx){
 
 }
 
+
+#Define PulsesPerRevolution 6;
+#Define MicrosecondsPerMinute 60000000.0;
+
+
+
 void ReadRpm(int Idx){
   TachoValue[Idx] = digitalRead(Idx+30);
   //Serial.println(TachoValue[Idx]);
 if(TachoValue[Idx] != OldTachoValue[Idx]){
   Rpm1[Idx] = micros()-TachoTimer[Idx];
-  Rpm3[Idx] = (1.0/(((Rpm1[Idx]*0.5+Rpm2[Idx]*0.5)/6000000.0)));
-  Rpm[Idx] = 0.00021299060075323*pow(Rpm[Idx],2.0)+0.74515357139003*Rpm[Idx]+96.860745896597;
+  Rpm3[Idx] = Rpm1[Idx]*0.5+Rpm2[Idx]*0.5;
+  //rpm3 is how many microseconds per pulse
+  
+  Rpm4[Idx] = Rpm3[Idx]*PulsesPerRevolution;
+  //rpm4 is how many microseconds per revolution
+
+  Rpm5[Idx] = MicrosecondsPerMinute/Rpm4[Idx];
+  // divide how many microseconds per minute by how many microseconds per revo to get rotations per minute
+      
   TachoTimer[Idx] = micros();
   Rpm2[Idx] = Rpm1[Idx];
-  Serial.println(Rpm[Idx]);
+  Serial.println(Rpm5[Idx]);
   }
   OldTachoValue[Idx] = TachoValue[Idx];
+  if(TachoTimer[Idx]<micros()-500000){TachoTimer[Idx]=micros();}
 }
 
 void ReadPot(int Pin,int Idx){
