bb72cd9cd30cacd1a0fb7f06d5d55ded6dc8cec9
Legomonster33 <siglevan@gmail.com>
2023-04-23 00:32:14 -0400

    Update PID_CONTROLLER.ino

--- PID_CONTROLLER/PID_CONTROLLER.ino
+++ PID_CONTROLLER/PID_CONTROLLER.ino
@@ -1,3 +1,32 @@
+float TachoTimer[8],TachovValue[8],OldTachoValue[8],Rpm[8],ControlValue[12],ProcessValue[12],SetPoint[12],P[12],I[12],D[12],MaxSummativeError[12],Error[12],LastError[12],ErrorSummative[12],PreviousTime[12],DeltaTime[12],Proportional,Integral,Derivative,
+
+void PID(Idx){
+  Error[Idx] = SetPoint - ProcessValue[Idx];
+  Proportional = P[Idx]*Error
+
+  ErrorSummative[Idx] = ErrorSummative[Idx] + Error[Idx]
+  if(ErrorSummative[Idx]>MaxSummativeError[Idx]){ErrorSummative[Idx]=MaxSummativeError[Idx]}
+  if(ErrorSummative[Idx]<MaxSummativeError[Idx]*-1){ErrorSummative[Idx]=MaxSummativeError[Idx]*-1}
+
+  DeltaTime[Idx] = mircos() - PreviousTime[Idx];
+
+  Integral = DeltaTime[Idx]*I[Idx]*ErrorSummative[Idx];
+
+  Derivative = ((D[Idx]/DeltaTime[Idx])/(Error[Idx]-LastError[Idx]))
+
+  ControlValue[Idx] = Proportional + Integral + Derivative;
+
+}
+
+void ReadRpm(Idx){
+  TachoValue[Idx] = digitalRead(Idx)
+if(TachoValue[Idx] != OldTachoValue[Idx]){
+
+}
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,8 +36,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-
-
-
-
-i edit le code
