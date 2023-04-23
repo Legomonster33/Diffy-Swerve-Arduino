7a73abd9fac45176021ccc472c5f77aa3c35b3e5
Legomonster33 <siglevan@gmail.com>
2023-04-23 18:59:10 -0400

    Update PID_CONTROLLER.ino

--- PID_CONTROLLER/PID_CONTROLLER.ino
+++ PID_CONTROLLER/PID_CONTROLLER.ino
@@ -1,14 +1,14 @@
-float AvgMicrosPerPulse[8],
-MicrosPerRevolution[8],
-MotorOutPut[8],
-PotVal[8],
-Rpm[8],
-TachoDeltaTimePrevious[8],
-MicrosPerPulse[8],
-TachoTimer[8],
-TachoValue[8],
-OldTachoValue[8],
-TachoDeltaTime[8],
+float AvgMicrosPerPulse[12],
+MicrosPerRevolution[12],
+MotorOutPut[12],
+PotVal[12],
+Rpm[12],
+TachoDeltaTimePrevious[12],
+MicrosPerPulse[12],
+TachoTimer[12],
+TachoValue[12],
+OldTachoValue[12],
+TachoDeltaTime[12],
 ControlValue[12],
 ProcessValue[12],
 SetPoint[12],
@@ -27,12 +27,13 @@ Integral,
 Derivative;
 
 void PID(int Idx){
+  if(SetPoint[Idx]<0.0){ProcessValue[Idx]=ProcessValue[Idx]*(-1.0);}
   Error[Idx] = SetPoint[Idx] - ProcessValue[Idx];
   Proportional = P[Idx]*Error[Idx];
 
   ErrorSummative[Idx] = ErrorSummative[Idx] + Error[Idx];
   if(ErrorSummative[Idx]>MaxSummativeError[Idx]){ErrorSummative[Idx]=MaxSummativeError[Idx];}
-  if(ErrorSummative[Idx]<MaxSummativeError[Idx]*-1){ErrorSummative[Idx]=MaxSummativeError[Idx]*-1;}
+  if(ErrorSummative[Idx]<MaxSummativeError[Idx]*-1.0){ErrorSummative[Idx]=MaxSummativeError[Idx]*-1.0;}
 
   DeltaTime[Idx] = micros() - PreviousTime[Idx];
 
@@ -40,9 +41,9 @@ void PID(int Idx){
 
   Derivative = ((D[Idx]/DeltaTime[Idx])/(Error[Idx]-LastError[Idx]));
 
-  ControlValue[Idx] = Proportional + Integral + Derivative;
+  ControlValue[Idx] = -1.0*(Proportional + Integral + Derivative);
   if(ControlValue[Idx]>MaxControl[Idx]){ControlValue[Idx]=MaxControl[Idx];}
-  if(ControlValue[Idx]<-1*MaxControl[Idx]){ControlValue[Idx]=-1*MaxControl[Idx];}
+  if(ControlValue[Idx]<-1.0*MaxControl[Idx]){ControlValue[Idx]=-1.0*MaxControl[Idx];}
 }
 
 
@@ -53,7 +54,7 @@ void PID(int Idx){
 
 void ReadRpm(int Idx){
   TachoValue[Idx] = digitalRead(Idx+30);
-  //Serial.println(TachoValue[Idx]);
+  //Serial.print(TachoValue[Idx]);
 if(TachoValue[Idx] != OldTachoValue[Idx]){
   TachoDeltaTime[Idx] = micros()-TachoTimer[Idx];  
   AvgMicrosPerPulse[Idx] = TachoDeltaTime[Idx]*0.5+TachoDeltaTimePrevious[Idx]*0.5;
@@ -67,36 +68,67 @@ if(TachoValue[Idx] != OldTachoValue[Idx]){
       
   TachoTimer[Idx] = micros();
   TachoDeltaTimePrevious[Idx] = TachoDeltaTime[Idx];
-  //Serial.println(Rpm[Idx]);
   }
   OldTachoValue[Idx] = TachoValue[Idx];
 }
 
 void ReadPot(int Pin,int Idx){
   PotVal[Idx] = PotVal[Idx]*0.99+0.01*((((analogRead(Pin)/512.0)-1.0)));
-  //Serial.println(PotVal[Idx]);
 }
 
 void MotorOutPutCalc(int Idx){
   MotorOutPut[Idx] = MotorOutPut[Idx] + MotorOutPut[Idx]*ControlValue[Idx];
-  if(MotorOutPut[Idx]<250){MotorOutPut[Idx]=250;}
-  if(MotorOutPut[Idx]>60){MotorOutPut[Idx]=60;}  
-  //Serial.println(MotorOutPut[Idx]);
+  if(MotorOutPut[Idx]>250){MotorOutPut[Idx]=250.0;Serial.print("maxxed");}
+  if(MotorOutPut[Idx]<70){MotorOutPut[Idx]=70.0;Serial.print("minned");} 
 }
 
 void PotRead(int PinNum,int Idx){
   PotVal[Idx] = PotVal[Idx]*0.9+0.1*(((analogRead(PinNum)-512.0)/512.0));
-  Serial.println(PotVal[Idx]);
-  //Serial.println(analogRead(PinNum));
 }
+
+void PrintAll(int Idx){
+
+
+
+  Serial.print("SetPoint_: ");
+  Serial.print(SetPoint[Idx]);
+  Serial.print("\t");
+  
+  Serial.print("ProcessValue_: ");
+  Serial.print(ProcessValue[Idx]);
+  Serial.print("\t");
+
+  Serial.print("ControlValue_: ");
+  Serial.print(ControlValue[Idx]);
+  Serial.print("\t");
+
+  Serial.print("MotorOutPut_: ");
+  Serial.print(MotorOutPut[Idx]);
+  Serial.print("\t");
+
+  Serial.print("Error_: ");
+  Serial.print(Error[Idx]);
+  Serial.print("\t");
+
+  Serial.print("Rpm_: ");
+  Serial.print(Rpm[Idx]);
+  Serial.print("\t");
+
+  Serial.print("TargetRpm_: ");
+  Serial.print(ProcessValue[Idx]*5000.0);
+  Serial.print("\t");  
+
+  Serial.println();
+}
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(1000000);
   MotorOutPut[0]=160;
-  P[0]=1;
-  I[0]=0;
-  D[0]=0;
+  P[0]=0.01;
+  I[0]=0.0;
+  D[0]=0.0;
   MaxSummativeError[0] = 1;
   MaxControl[0]=1;
   
@@ -108,6 +140,8 @@ void loop() {
   MotorOutPutCalc(0);
   PotRead(A0,0);
   analogWrite(2, MotorOutPut[0]);
-  ProcessValue[0] = Rpm[0]/6000;
+  ProcessValue[0] = Rpm[0]/5000.0;
   SetPoint[0]=PotVal[0];
+  PID(0);
+  PrintAll(0);
 }
