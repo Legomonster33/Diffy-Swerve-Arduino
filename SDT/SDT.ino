16496b8ee073a20a45d813820ded1733b5fc6556
ksigl <karljsigl@gmail.com>
2023-05-02 14:35:35 -0400

    Update SDT.ino

--- SDT/SDT.ino
+++ SDT/SDT.ino
@@ -3,6 +3,9 @@
 #include  <math.h>
 
 #define pi  3.141592654
+#define AI_SPAN           1023
+#define JOY_STICK_SPAN    200
+#define JOY_STICK_EGL     100
 
 #define DT_TRACK_WIDTH    100
 #define DT_WHEEL_BASE     100
@@ -18,10 +21,10 @@
 #define JOYSTICK_ROTATE_IN  A2
 
 struct joystick{
-  float yAXISraw;             // analog input from joystick 1 for forward and reverse
-  float xAXISraw;             // analog input from joystick 1 for left and right
-  float frREQ;
-  float lrREQ;
+  int     yAXISraw;             // analog input from joystick 1 for forward and reverse
+  int     xAXISraw;             // analog input from joystick 1 for left and right
+  float  yAXISreq;
+  float  xAXISreq;
 };
 
 struct  swerveModule {
@@ -52,12 +55,8 @@ void  read_joysticks() {
   joy1.yAXISraw = analogRead(JOYSTICK_MOVE_IN);       // raw analog input for Forward/Reverse
   joy1.xAXISraw = analogRead(JOYSTICK_STRAFE_IN);   // raw analog input for STRAFE
 
-  joy1.frREQ = (joy1.yAXISraw/1023)*200;
-  joy1.lrREQ = (joy1.xAXISraw/1023)*200;
-  // wtf  
-  sprintf(disp,"%f %f", joy1.yAXISraw, joy1.xAXISraw);
-  Serial.println(disp);
-
+  joy1.yAXISreq = (float(joy1.yAXISraw)/AI_SPAN) * JOY_STICK_SPAN - JOY_STICK_EGL;
+  joy1.xAXISreq = (float(joy1.xAXISraw)/AI_SPAN) * JOY_STICK_SPAN - JOY_STICK_EGL;
 }
 
 void  calc_module_ctrl(float FWD, float STR, float RCW, float GYRO) {
@@ -95,10 +94,13 @@ void  calc_module_ctrl(float FWD, float STR, float RCW, float GYRO) {
   ws3 = sqrt(A*A + D*D);
   ws4 = sqrt(A*A + C*C);
 
-  wa1 = atan2(B,C) * 180/pi;
-  wa2 = atan2(B,D) * 180/pi;
-  wa3 = atan2(A,D) * 180/pi;
-  wa4 = atan2(A,C) * 180/pi;
+  wa1 = atan2(B,C) * 180.0/pi -45;
+  wa2 = atan2(B,D) * 180.0/pi -45;
+  wa3 = atan2(A,D) * 180.0/pi -45;
+  wa4 = atan2(A,C) * 180.0/pi -45;
+
+  sprintf(disp,"FWD:%3d STR:%3d Wheel1(%3d,%3d) Wheel2(%3d,%3d) Wheel3(%3d,%3d) Wheel4(%3d,%3d)",int(FWD),int(STR),int(ws1),int(wa1),int(ws2),int(wa2),int(ws3),int(wa3),int(ws4),int(wa4));
+  Serial.println(disp);
 
   max_ws = ws1;
   if (ws2 > max_ws) max_ws = ws2;
@@ -118,13 +120,24 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+  pinMode(5,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  read_joysticks();
-  calc_module_ctrl(joy1.frREQ, joy1.lrREQ, 0, 0);
-  delay(200);
+  int yVAL, xVAL;
 
+  read_joysticks();
+  calc_module_ctrl(joy1.yAXISreq, joy1.xAXISreq, 0, 0);
+
+  yVAL = analogRead(A0);
+  xVAL = analogRead(A1);
+  
+  analogWrite(MOD1_SUN_MOTOR_OUT,yVAL/1023.0*180.0+65);
+  analogWrite(MOD1_RING_MOTOR_OUT,xVAL/1023.0*180.0+65);
+  analogWrite(MOD2_SUN_MOTOR_OUT,yVAL/1023.0*180.0+65);
+  analogWrite(MOD2_RING_MOTOR_OUT,xVAL/1023.0*180.0+65);
+  
+  
 }
